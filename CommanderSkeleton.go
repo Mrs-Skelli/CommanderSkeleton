8f5bb3e1e1e44f026ef36a3f8933ffef5d404f0f
Eden S <EdenStroet@gmail.com>
2021-09-19 00:35:52 +0200

    Updated some things, added a command with syntax.
    Co-Authored-By: 9o3 <bugbot6@gmail.com>

--- CommanderSkeleton.go
+++ CommanderSkeleton.go
@@ -2,20 +2,20 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	//"fmt"
 	"log"
 	"os"
 	"os/signal"
 	"strings"
-	"time"
-
+	//"time"
+	"syscall"
 	"github.com/bwmarrin/discordgo"
 )
 
 
 var (
 	GuildID        = flag.String("guild", "", "Test guild ID. If not passed - bot registers commands globally")
-	BotToken       = flag.String("token", "", "Bot access token")
+//	BotToken       = flag.String("token", "", "Bot access token")
 	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
 )
 
@@ -24,24 +24,25 @@ var s *discordgo.Session
 func init() { flag.Parse() }
 
 func init() {
-	var err error
-	token, err := os.LookupEnv("TOKEN")
-	if err {//We also parse command line args line 18,
-		// y
-		log.Fatalf("TOKEN Not set")
+	token, isset := os.LookupEnv("TOKEN")
+	if (!isset) {
+		log.Fatalf("TOKEN is not set")
 	}
-	s, err = discordgo.New("Bot " + *BotToken)
+	var err error
+	s, err = discordgo.New("Bot " + token)
 	if err != nil {
 		log.Fatalf("Invalid bot parameters: %v", err)
 	}
 }
 
-var (
+var(
 	commands = []*discordgo.ApplicationCommand{
 		{
 			Name: "basic-command",
-			Description: "Basic command"}}
-  commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
+			Description: "Basic command",
+		},
+	}
+	commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate){
 		"basic-command": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
@@ -49,9 +50,23 @@ var (
 					Content: "Hey there! Congratulations, you just executed your first slash command",
 				},
 			})
-		}
+		},
   }
 )
+
+func message_handler(s *discordgo.Session, m *discordgo.MessageCreate){
+	log.Println(m.Author.ID)
+	if m.Author.ID == s.State.User.ID {
+        return
+  }
+
+	 var tokenized []string = strings.Split(m.Content, " ")
+	 // Define Commands here
+	 switch tokenized[0] {
+	 case "!Hi":
+		 s.ChannelMessageSend(m.ChannelID, "Hello! This is a test, and will not be working in the future.")
+	 }
+}
 
 func init() {
 	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
@@ -70,6 +85,8 @@ func main() {
 		log.Fatalf("Cannot open the session: %v", err)
 	}
 
+	s.AddHandler(message_handler)
+	//log.Println(len(commands))
 	for _, v := range commands {
 		_, err := s.ApplicationCommandCreate(s.State.User.ID, *GuildID, v)
 		if err != nil {
@@ -79,8 +96,8 @@ func main() {
 
 	defer s.Close()
 
-	stop := make(chan os.Signal)
-	signal.Notify(stop, os.Interrupt)
-	<-stop
+	sc := make(chan os.Signal, 1)
+  signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+  <-sc
 	log.Println("Gracefully shutdowning")
 }
